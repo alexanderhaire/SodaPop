b96f41a09919f327d3d0a1ce31031effeaf781b9
Alexander William Haire <141470777+alexanderhaire@users.noreply.github.com>
2025-06-18 00:46:45 -0400

    Enhance UI styling

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -5,7 +5,7 @@ import Dashboard from "../components/Dashboard";
 const DashboardPage: React.FC = () => {
   return (
     <Container maxW="4xl" py={10}>
-      <Box bg="white" p={6} borderRadius="xl" shadow="lg">
+      <Box bg="whiteAlpha.800" p={6} borderRadius="xl" boxShadow="lg">
         <Dashboard userAddress="0x1462...DBee" />
       </Box>
     </Container>
